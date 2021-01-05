b6138bfb4f071fde54eeac1f2cbb6fdc42815425
Anjewka <57514498+Anjewka@users.noreply.github.com>
2021-01-05 14:15:59 +0300

    Add files via upload

---
+++ game.h
@@ -0,0 +1,58 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QWidget>
+#include <QVector>
+#include <QPoint>
+#include <QKeyEvent>
+#include <QPushButton>
+#include <QLabel>
+#include <QTimer>
+#include <block.h>
+#include <food.h>
+#include <QGridLayout>
+
+class Game : public QWidget
+{
+private:
+    double Width = 30;
+    double Height = 30;
+    double field_Width = 30;
+    double field_Height = 30;
+
+    int score;
+
+    int direction;
+    bool play;
+    bool menu;
+    QVector<Block*> snake;
+    Food* fruit;
+
+    QPushButton* bt1;
+    QPushButton* bt2;
+
+    QTimer* t1;
+    QTimer* t2;
+
+public:
+    Game();
+    void initGame();
+    void Draw();
+    void Move();
+    void Check();
+    void gameOver();
+    void drawMenu();
+    void reDraw();
+    void eat();
+    void checkSnake();
+    void restartGame();
+    void pause();
+    void upd();
+    void timeEvent();
+
+protected:
+    void keyPressEvent(QKeyEvent *) override;
+    void paintEvent(QPaintEvent *event) override;
+};
+
+#endif // GAME_H
