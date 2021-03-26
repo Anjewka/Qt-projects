e29596745b13cf72dcf6408ed8665b0ca56d56f0
Andrew <57514498+Anjewka@users.noreply.github.com>
2021-03-26 15:51:58 +0300

    Update block.h

--- snake/block.h
+++ snake/block.h
@@ -1,25 +1,20 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-#include <QColor>
-
 class Block
 {
 private:
     int x;
     int y;
-    QColor color;
 
 public:
     Block();
-    Block(int x1, int y1, QColor c);
+    Block(int x1, int y1);
 
     virtual int getX();
     virtual int getY();
-    virtual QColor getColor();
     virtual void setX(int x1);
     virtual void setY(int y1);
-    virtual void setColor(QColor c);
 };
 
 #endif // BLOCK_H
