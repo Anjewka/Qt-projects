e120fe224e7a7194cbc5edb8db27cf60fbd10ef7
Andrew <57514498+Anjewka@users.noreply.github.com>
2021-03-26 15:51:22 +0300

    Update block.cpp

--- snake/block.cpp
+++ snake/block.cpp
@@ -1,19 +1,16 @@
 #include "block.h"
-#include <QColor>
 #include <random>
 #include <ctime>
 
 Block::Block()
 {
     x = y = 0;
-    color = Qt::green;
 }
 
-Block::Block(int x1, int y1, QColor c)
+Block::Block(int x1, int y1)
 {
     x = x1;
     y = y1;
-    color = c;
 }
 
 int Block::getX()
@@ -26,11 +23,6 @@ int Block::getY()
     return y;
 }
 
-QColor Block::getColor()
-{
-    return color;
-}
-
 void Block::setX(int x1)
 {
     x = x1;
@@ -39,9 +31,4 @@ void Block::setX(int x1)
 void Block::setY(int y1)
 {
     y = y1;
-}
-
-void Block::setColor(QColor c)
-{
-    color = c;
 }
