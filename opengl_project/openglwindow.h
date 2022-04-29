ea9fab15a28beb2fc23eb868bfca8dcf3caca647
Andrew <57514498+Anjewka@users.noreply.github.com>
2022-04-30 00:01:27 +0300

    Add files via upload

---
+++ opengl_project/openglwindow.h
@@ -0,0 +1,50 @@
+#ifndef OPENGLWINDOW_H
+#define OPENGLWINDOW_H
+
+#include <QWindow>
+#include <QOpenGLFunctions>
+#include <QWidget>
+#include <QPainter>
+#include <QLineEdit>
+#include <QOpenGLExtraFunctions>
+#include <QApplication>
+#include <QMouseEvent>
+#include <QLineEdit>
+
+QT_BEGIN_NAMESPACE
+class QPainter;
+class QOpenGLContext;
+class QOpenGLPaintDevice;
+QT_END_NAMESPACE
+
+class OpenGLWindow : public QWindow, protected QOpenGLExtraFunctions
+{
+    Q_OBJECT
+public:
+    explicit OpenGLWindow(QWindow *parent = nullptr);
+    ~OpenGLWindow();
+
+    virtual void render(QPainter *painter);
+
+    virtual void render();
+
+    virtual void initialize();
+
+    void setAnimating(bool animating);
+
+public slots:
+    void renderLater();
+    void renderNow();
+
+protected:
+    bool event(QEvent *event) override;
+
+    void exposeEvent(QExposeEvent *event) override;
+
+    bool m_animating = false;
+
+    QOpenGLContext *m_context = nullptr;
+    QOpenGLPaintDevice *m_device = nullptr;
+};
+
+#endif // OPENGLWINDOW_H
